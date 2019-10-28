574a6bc2b0132357854a5371999ee10d1d0ca7ec
Agata Paldyna <paldyna.agata@gmail.com>
2019-10-28 23:33:25 +0100

    Added validation of input data

--- src/input_data_validator.py
+++ src/input_data_validator.py
@@ -1,2 +1,38 @@
-def validate_input_data(seq1, seq2, config):
+from needleman_wunsch_algorithm_config import Needleman_wunsch_algorithm_config
+import constants
+
+
+def validate_input_data(seq1, seq2, config: Needleman_wunsch_algorithm_config):
+    validation_result = validate_config(config)
+    if not validation_result[0]:
+        return validation_result
+
+    if len(seq1) > config.MAX_SEQ_LENGTH or len(seq2) > config.MAX_SEQ_LENGTH:
+        return False, f"Given sequences are too long! Maximum possible length of sequences is {config.MAX_SEQ_LENGTH}"
+
     return True, ""
+
+
+def validate_config(config: Needleman_wunsch_algorithm_config):
+    parameters_names_list = ["SAME", "DIFF", "GP", "MAX_NUMBER_PATHS", "MAX_SEQ_LENGTH"]
+
+    for param_name in parameters_names_list:
+        if not isinstance(config[param_name], int):
+            return False, f"Parameter {param_name} is not int!"
+            
+    for param_name in parameters_names_list[0:3]:
+        if config[param_name] == 0:
+            return False, f"Parameter {param_name} can not be equal to 0!"
+
+    for param_name in parameters_names_list[3:]:
+        if config[param_name] < 1:
+            return False, f"Parameter {param_name} can not be less than 1!"
+
+    if config.SAME <= config.DIFF:
+        return False, f"Parameter SAME must be greater than parameter DIFF!"
+
+    if config.MAX_SEQ_LENGTH > constants.MAXIMUM_SEQ_LEN:
+        return False, f"Value of parameter MAX_SEQ_LENGTH is too big. It should be less than {constants.MAXIMUM_SEQ_LEN}"
+
+    if config.MAX_NUMBER_PATHS > constants.MAXIMUM_NUMBER_PATHS:
+        return False, f"Value of parameter MAX_NUMBER_PATHS is too big. It should be less than {constants.MAXIMUM_NUMBER_PATHS}"
--- src/needleman_wunsch_algorithm_config.py
+++ src/needleman_wunsch_algorithm_config.py
@@ -4,8 +4,11 @@ from config import Config
 class Needleman_wunsch_algorithm_config(Config):
     def __init__(self, config_file = "config.txt"):
         Config.__init__(self, config_file)
-        self.SAME = self["SAME"]
-        self.DIFF = self["DIFF"]
-        self.GP = self["GP"]
-        self.MAX_NUMBER_PATHS = self["MAX_NUMBER_PATHS"]
-        self.MAX_SEQ_LENGTH = self["MAX_SEQ_LENGTH"]
+        try:
+            self.SAME = self["SAME"]
+            self.DIFF = self["DIFF"]
+            self.GP = self["GP"]
+            self.MAX_NUMBER_PATHS = self["MAX_NUMBER_PATHS"]
+            self.MAX_SEQ_LENGTH = self["MAX_SEQ_LENGTH"]
+        except KeyError as ke:
+            raise Exception(f"Key {ke} is missing in config file")
